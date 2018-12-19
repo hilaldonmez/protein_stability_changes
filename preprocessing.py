c4f608393596d8f0bc4534b7ee9446cdf8eb777f
hilal <hilaldnmez@yahoo.com>
2018-12-20 01:16:06 +0300

    aminoacid dict

--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,6 @@
-dataset_path = "./data/s1615.txt"
+import numpy as np
+s1615_dataset_path = "./data/s1615.txt"
+s388_dataset_path = "./data/s388.txt"
 
 # 1615 mutations
 # nested list
@@ -13,6 +15,7 @@ def read_dataset(dataset_path):
         inner_array = []
         for line in f:
             if not ((line.startswith("#") or len(line.strip()) == 0))  :
+               line = line.replace("\n", '')
                if count == 8 :
                    properties = line.split(" ")
                    properties[2] = int(properties[2]) 
@@ -35,20 +38,42 @@ def read_dataset(dataset_path):
                elif count != 9:
                    inner_array.append(line)
                    count = count + 1
+
     return all_mutations
 
-all_mutations = read_dataset(dataset_path)
- 
 #%%
-temp_mutations = all_mutations.copy()
-remove_index = []
-for i in range(len(temp_mutations)):
-    for j in range(i+1,len(temp_mutations)):
-        if (temp_mutations[i][-1][5] == temp_mutations[j][-1][5]) and (temp_mutations[i][-1][6] == temp_mutations[j][-1][6]) and (temp_mutations[i][-1][0] == temp_mutations[j][-1][0]) and (temp_mutations[i][-1][1] == temp_mutations[j][-1][1]) and (temp_mutations[i][-1][2] == temp_mutations[j][-1][2]) and (temp_mutations[i][-1][3] == temp_mutations[j][-1][3]) and (temp_mutations[i][-1][4] == temp_mutations[j][-1][4]) :
-        
-            remove_index.append(j)
- 
-remove_index = list(set(remove_index))    
-    
+# generate s1496 dataset after removing duplicate mutations
+def generate_s1496_dataset(all_mutations):
+    remove_index = []
+    for i in range(len(all_mutations)):
+        for j in range(i+1,len(all_mutations)):
+            if (all_mutations[i][-1][5] == all_mutations[j][-1][5]) and (all_mutations[i][-1][6] == all_mutations[j][-1][6]) and (all_mutations[i][-1][0] == all_mutations[j][-1][0]) and (all_mutations[i][-1][1] == all_mutations[j][-1][1]) and (all_mutations[i][-1][2] == all_mutations[j][-1][2]) and (all_mutations[i][-1][3] == all_mutations[j][-1][3]) and (all_mutations[i][-1][4] == all_mutations[j][-1][4]) :
+            
+                remove_index.append(j)
+     
+    remove_index = list(set(remove_index))    
+    for index in sorted(remove_index, reverse=True):
+        del all_mutations[index]
+    return all_mutations  
+      
 #%%
+# mutation info vectors
+# X aminoacid is unimportant or unknown, remove X  
+# create dictionary with aminoacid abbreviations and id given
+def generate_aa_dict(s1615_mutations):
+    aa_dict = set(''.join([i[2] for i in s1615_mutations]))          
+    aa_dict.remove('X')
+    d = {value: index for value,index in zip(aa_dict,np.arange(len(aa_dict))) }
+    return d
+
+#%%
+s1615_mutations = read_dataset(s1615_dataset_path)
+s388_mutations = read_dataset(s388_dataset_path)
+s1496_mutations = generate_s1496_dataset(s1615_mutations.copy())    
+aa_dict = generate_aa_dict(s1615_mutations)
+
  
+
+
+
+
